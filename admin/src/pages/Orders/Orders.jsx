ea8327781186b47e5ac87e48688c4772f047c076
AD\barbog <bartlomiej.bogusz@hidglobal.com>
2024-05-02 10:23:59 +0200

    final commit

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -21,6 +21,16 @@ const Orders = ({url}) => {
     }
   }
 
+  const statusHandler = async (event,orderId) => {
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -44,7 +54,19 @@ const Orders = ({url}) => {
                 })}
               </p>
               <p className="order-item-name">{order.address.firstName+" "+order.address.lastName}</p>
+              <div className="order-item-address">
+                <p>{order.address.street+","}</p>
+                <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+              </div>
+              <p className='order-item-phone'>{order.address.phone}</p>
             </div>
+            <p>Items : {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
