2c7f7719565a2c04e99bb89e1a95467ac2ec8769
AD\barbog <bartlomiej.bogusz@hidglobal.com>
2024-06-07 20:00:48 +0200

    latest changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,12 +6,11 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 
-    const [menu,setMenu] = useState("menu");
+    const [menu,setMenu] = useState("home");
 
     const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
     const navigate = useNavigate();
-
     const logout = () => {
       localStorage.removeItem("token");
       setToken("");
